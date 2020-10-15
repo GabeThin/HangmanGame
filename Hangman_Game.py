967ca75590f3dfe5b7c908d8e9586e87054a4c15
hankelse <72819191+hankelse@users.noreply.github.com>
2020-10-15 11:21:00 -0400

    Update Hangman_Game.py

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -30,7 +30,8 @@ def handle_guess():
     if len(letter) == 1:
         if letter in word:
             print("correct")
-            blanks = Label(root, textvariable= display)
+            display = ""
+            blanks = Label(root, textvariable=display)
             blanks.pack(padx=(100, 0), side=LEFT)
             display.set(display_word(secret_word, chosen_letters))
         else:
