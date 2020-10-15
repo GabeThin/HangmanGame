a9cd13f2828848ae81ecfae35b7c2fc040946591
hankelse <72819191+hankelse@users.noreply.github.com>
2020-10-14 21:29:15 -0400

    Update Hangman_Game.py

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -97,10 +97,12 @@ def timed_GUI():
   secret_word = "the"
   submit = Button(frame, text = "Submit Guess", font = ("Arial", 24), command=check_guess())
   submit.pack(pady = (350, 0), padx = 100, side = LEFT)
+  def check_guess():
+    letter = guess.get()
+    return letter
   
   
-def check_guess():
-  letter = guess.get()
+
 
 def main_timed():
   for i in root.winfo_children():
